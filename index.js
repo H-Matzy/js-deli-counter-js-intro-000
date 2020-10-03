c155ff1804faed0ed042d9a5a6f5110f0cb7e3dc
Hayden Matz <haydenmatz97@gmail.com>
2020-10-03 18:47:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,5 +10,10 @@ function nowServing(katzDeliLine){
 }
 function currentLine(katzDeliLine){
   if (katzDeliLine.length === 0) return "The line is currently empty."
-  else  return `The line is currently:`
+  else  {
+    var solution = 'The line is currently:'
+    for (var i==0; i=katzDeliLine.length; i++){
+      solution.push(i.name)
+    }
+    return `The line is currently:`
 }
