609e3e3dc379a2227a16786381736db6395addd6
Hayden Matz <haydenmatz97@gmail.com>
2020-10-03 18:04:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function takeANumber(katzDeliLine,name){
   katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
+function nowServing(katzDeliLine){
+  if {katzDeliLine.length < 1} return "There is nobody waiting to be served!"
+  else {katzDeliLine.shift()}
+}
