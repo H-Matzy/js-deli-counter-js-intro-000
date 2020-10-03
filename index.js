718b12bb52931c05647e63bd341121d3ee98b85e
Hayden Matz <haydenmatz97@gmail.com>
2020-10-03 19:52:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function currentLine(katzDeliLine){
   else  {
     var solution = 'The line is currently:'
     for (var i=0; i < katzDeliLine.length; i++){
-      solution += katzDeliLine[`${1}, ${2}, ${3}`]
+      solution += `katzDeliLine[1]`
     }
     return solution
 }
