ba65e792eea1f200c980cd911f3099b9e55044d5
Hayden Matz <haydenmatz97@gmail.com>
2020-10-03 18:36:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ function nowServing(katzDeliLine){
 }
 function currentLine(katzDeliLine){
   if (katzDeliLine.length === 0) return "The line is currently empty."
-  else  return "The line is currently:"
+  else  return `The line is currently: 1. katDeliLine[0]`
 }
