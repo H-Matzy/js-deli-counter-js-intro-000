332308129a370e1b47188248312b7d174ad767d9
Hayden Matz <haydenmatz97@gmail.com>
2020-10-03 17:53:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(katzDeliLine,name){
-  var lin = katzDeliLine.length
+  var line = katzDeliLine.length
   katzDeliLine = katzDeliLine[name]
-  return `Welcome, ${name}. You are number ${lin+1} in line.`
+  return `Welcome, ${name}. You are number ${line+1} in line.`
 }
